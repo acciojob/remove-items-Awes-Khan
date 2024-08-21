74b87f4b617e73293bd482543076a57583946e31
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-22 01:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+const submitBtn = document.getElementById('submit');
+submitBtn.addEventListener('click', () => {
+	const colorSelect = document.getElementById('colorSelect');
+    const selectedIndex = colorSelect.selectedIndex;    
+    if (selectedIndex !== -1)
+        colorSelect.remove(selectedIndex);
+});
+
